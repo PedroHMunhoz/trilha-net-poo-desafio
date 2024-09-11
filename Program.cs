8884c204b5dd532236858d2341e27b63b394f8fe
Pedro Henrique Munhoz Costa <pedromunhozc@gmail.com>
2024-09-11 20:23:52 -0300

    [Desafio Implementado]
    - Implementado as propriedades privadas na classe base Smartphone e
    transformada a classe em abstrata.
    - Implementado a herança nas classes Nokia e Iphone, com sobrescrita
    do método "InstalarAplicativo" e criação do construtor para popular
    o construtor da classe base.
    - Implementados alguns testes na program.
    
    Signed-off-by: Pedro Henrique Munhoz Costa <pedromunhozc@gmail.com>

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia n1 = new Nokia("123456789", "3310", "338537144037185", 4);
+n1.ReceberLigacao();
+n1.Ligar();
+n1.InstalarAplicativo("WhatsApp");
+
+Iphone i1 = new Iphone("987654321", "15 Max Pro", "981239707311988", 16);
+i1.ReceberLigacao();
+i1.Ligar();
+i1.InstalarAplicativo("WhatsApp");
